1d2d19dc0dc161a6897204502454f98b32e8dc09
Turku Game Lab <TGL@DC.local>
2018-04-24 16:02:36 +0300

    UI buttons added, AudioManager implementation started, Collede to objects works now

---
+++ Assets/Scripts/AudiManager_S.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudiManager_S : MonoBehaviour {
+
+    public Sound_S[] sounds;
+	// Use this for initialization
+	void Awake () {
+
+        foreach (Sound_S sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play(string name)
+    {
+      Sound_S s = System.Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+ 
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PlayerBehavior : MonoBehaviour {
+public class PlayerBehavior : MonoBehaviour, IPointerDownHandler,IPointerUpHandler {
 
    public GameObject player;
 
-    public int speed = 100;
+    public Button lButton;
+
+    public float speed = 0;
+
+    int counter = 0;
+
+    bool pointerDown;
+
+    float pointerDownTimer = 0.0f;
+
+    public UnityEvent ButtonHold;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +30,39 @@ public class PlayerBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        player.transform.Translate(Vector2.up * Time.deltaTime*20);
+        
+
+        if (counter == 10){
+            
+        }
+        else{
+
+        speed += 0.01f;
+        }
+        player.transform.Translate(0,0.1f*Time.deltaTime*speed,0);
+        Debug.Log(speed);
 	}
+
+   public void GoLeft()
+    {
+
+        player.transform.Translate(Vector2.left*Time.deltaTime*10);
+    }
+
+   public void GoRight()
+    {
+        player.transform.Translate(Vector2.right * Time.deltaTime);
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointerDown = false;
+    }
 }
